79df1b091926bd70b34cd6f1c1e46ee80a16d2ff
Nikita Laptev <nikita.laptev98@inbox.ru>
2017-10-11 12:16:23 +0300

    all lab.work is done

--- create.js
+++ create.js
@@ -1,8 +1,14 @@
 const log = require('./log.js');
 const file = 'logfile.json';
+const ErrorObject = { code: 400, message: 'Request Invalid' };
 let articles = require('./articles.json');
 
 module.exports.create = function create(req, res, payload, cb) {
+    if (!payload) 
+    {
+        cb(ErrorObject);
+        return;
+    }
     payload.id = Date.now();
     articles.push(payload);
     log.log(file, '/api/articles/create', payload);
